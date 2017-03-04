5251df487c04d844d6646b073eddc50c50af91f5
LAPTOP-O5DNB7D8\Nimmi <nimmiw@99x.lk>
2017-03-04 20:16:28 +0530

    - add extension files

---
+++ background.js
@@ -0,0 +1,63 @@
+var timestamp = new Date().getTime;
+
+var number_of_opened_tabs = 0;
+
+var tabList = [];
+
+
+chrome.runtime.onMessage.addListener(function(response,sender,sendResponse){
+    alert(response);
+});
+
+chrome.browserAction.onClicked.addListener(function(){
+    chrome.tabs.create({url:chrome.extension.getURL('hello.html')});
+});
+
+chrome.tabs.onCreated.addListener(function (tab){
+    number_of_opened_tabs = number_of_opened_tabs + 1;
+    //console.log('total opened tabs: ' + number_of_opened_tabs);
+    var t = {
+        id : tab.id,
+        activetime : 0,
+        isActive : false,
+        lastActivatedtime : Number(new Date().getTime())
+    };
+    tabList.push(t);
+    console.log('a new Tab created : ' + tab.id);
+})
+
+chrome.tabs.onRemoved.addListener(function (id){
+    number_of_opened_tabs = number_of_opened_tabs - 1;
+    //console.log('total opened tabs: ' + number_of_opened_tabs);
+    for (var index in tabList){
+        if(tabList[index].id == id){
+            tabList.splice( index, 1 );
+        }
+    }
+    console.log('a Tab was closed : ' + id);
+})
+
+chrome.tabs.onActivated.addListener(function (tab){
+    //console.log('Tab activated : ')
+    //console.log(tab);
+    for (var index in tabList){
+        if(tabList[index].isActive == true){
+            tabList[index].isActive = false;
+            tabList[index].lastActivatedtime = Number(new Date().getTime());
+            console.log('a tab went inactive : ' + tabList[index].id);
+        }
+        
+    }
+    for (var index in tabList){
+        if(tabList[index].id == tab.tabId){
+            var currTimestamp = Number(new Date().getTime())
+            var difference = currTimestamp - tabList[index].lastActivatedtime;
+            tabList[index].activetime = tabList[index].activetime + difference;
+            tabList[index].isActive = true;
+            console.log('a tab came active : ' + tabList[index].id);
+            console.log(tabList[index].activetime);
+        }
+        
+    }
+    
+})
---
+++ popup.js
@@ -0,0 +1,10 @@
+
+alert(httpGet('https://en.wikipedia.org/wiki/Main_Page'));
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
